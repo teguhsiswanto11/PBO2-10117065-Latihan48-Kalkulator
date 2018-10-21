5023f989d76fef0dde913524e24b89db670fc43f
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-21 08:48:49 +0700

    menambahkan deskripsi program

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
  * NAMA     : Teguh Siswanto
  * KELAS    : PBO2
  * NIM      : 10117065
- * Deskripsi Program :
+ * Deskripsi Program : Menghitung operasi kabataku(kali bagi tambah kurang)
+ * dengan nilai yang telah ditentukan
  *
  */
 
