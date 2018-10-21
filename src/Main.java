b6ed89e6ec36696e092279930becb06365635291
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-21 08:30:05 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author
+ * NAMA     : Teguh Siswanto
+ * KELAS    : PBO2
+ * NIM      : 10117065
+ * Deskripsi Program :
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Kalkulator kal = new Kalkulator();
+        kal.setValue1(7);
+        kal.setValue2(5);
+
+        System.out.println("VALUE 1 = "+kal.getValue1());
+        System.out.println("VALUE 2 = "+kal.getValue2());
+        kal.setNameProject();
+        kal.setNoteProject("This project shown you how to manage method in java");
+        System.out.println("Result Add is = "+kal.add(kal.getValue1(),kal.getValue2()));
+        System.out.println("Result Minus is = "+kal.minus(kal.getValue1(),kal.getValue2()));
+        System.out.println("Result Multiplication is = "+kal.multiplication(kal.getValue1(),kal.getValue2()));
+        System.out.println("Result Division is = "+kal.division(kal.getValue1(),kal.getValue2()));
+
+    }
+}
